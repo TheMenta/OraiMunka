a9cd50bb1167f288ae889e69c16eebfc9f86e7e1
thementa <thementaaa@gmail.com>
2023-01-31 10:54:17 +0100

    pont szamlalas

--- main.py
+++ main.py
@@ -3,4 +3,12 @@ def eredmeny():
     J_lapok_szama = 22
     if J_lapok_szama > 21:
         print("jatekos vesztett")
+    elif J_lapok_szama > 15:
+        print("Jatekos nyert")
+def lapok_osszege():
+    lapok = 0
+    pontok: int = 0
+    for i in range(len(lapok)):
+        pontok += lapok[i]
+    return
 #teszt esetek
