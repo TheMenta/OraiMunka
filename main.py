e41c5a31c35c8619b5719607988e3f1599a8a17a
thementa <thementaaa@gmail.com>
2023-02-02 16:54:07 +0100

    Teszt esetek v4

--- main.py
+++ main.py
@@ -2,13 +2,24 @@
 def eredmeny(g_lapok:[int],j_lapok:[int]):
     j_pontok = lapok_osszege(j_lapok)
     g_pontok = lapok_osszege(g_lapok)
+    allapot = " "
     if j_pontok > 21:
         allapot ="jatekos vesztett"
     elif g_pontok > 21:
         allapot = "gep vesztett"
+    elif j_pontok == g_pontok:
+        allapot = "dontetlen"
+    elif j_pontok == 21:
+        allapot = "jatekos nyert"
+    elif g_pontok == 21:
+        allapot = "gep nyert"
+    elif 21 > g_pontok > j_pontok:
+        allapot = "gep nyert"
+    elif 21 > j_pontok > g_pontok:
+        allapot = "jatekos nyert"
     return allapot
 
-def lapok_osszege(lapok:[int]):
+def lapok_osszege(lapok: [int]):
     pontok: int = 0
     for i in range(len(lapok)):
         pontok += lapok[i]
@@ -16,17 +27,21 @@ def lapok_osszege(lapok:[int]):
 #teszt esetek
 def teszt_esetek():
     jatekos_vesztett_teszt()
-
+    jatekos_kisebblapok_teszt()
+    gep_vesztett_teszt()
+    gep_kisebblapok_teszt()
+    dontetlen_teszt()
 def jatekos_vesztett_teszt():
     jatekos = [10,9,3]
     gep = [10,9]
     vart_eredmeny ="jatekos vesztett"
     kapott_eredmeny = eredmeny(gep,jatekos)
+    print("ez, itt a jatekos vesztett,tul-21")
     if kapott_eredmeny == vart_eredmeny:
         print("teszt sikeres")
     else:
         print("nem sikerult")
-teszt_esetek()
+
 def gep_vesztett_teszt():
     jatekos = [10,9]
     gep = [10,9,3]
@@ -36,7 +51,6 @@ def gep_vesztett_teszt():
         print("teszt sikeres")
     else:
         print("nem sikerult")
-teszt_esetek()
 
 
 def dontetlen_teszt():
@@ -48,25 +62,35 @@ def dontetlen_teszt():
         print("teszt sikeres")
     else:
         print("nem sikerult")
-teszt_esetek()
-def jatekos_kisebblapok_teszt():
-    jatekos = [10,2]
-    gep = [10,9,]
-    vart_eredmeny ="jatekos vesztett"
+def dontetlen_teszt1():
+    jatekos = [10, 3, 6, 2]
+    gep = [10, 5, 4, 2]
+    vart_eredmeny ="dontetlen"
     kapott_eredmeny = eredmeny(gep,jatekos)
     if kapott_eredmeny == vart_eredmeny:
         print("teszt sikeres")
     else:
         print("nem sikerult")
-teszt_esetek()
+
+def jatekos_kisebblapok_teszt():
+    jatekos = [10,6]
+    gep = [10,9]
+    vart_eredmeny ="gep nyert"
+    kapott_eredmeny = eredmeny(gep,jatekos)
+    if kapott_eredmeny == vart_eredmeny:
+        print("teszt sikeres")
+    else:
+        print("nem sikerult")
+
 
 def gep_kisebblapok_teszt():
     jatekos = [10,9,]
     gep = [10,2]
-    vart_eredmeny ="gep vesztett"
+    vart_eredmeny ="jatekos nyert"
     kapott_eredmeny = eredmeny(gep,jatekos)
     if kapott_eredmeny == vart_eredmeny:
         print("teszt sikeres")
     else:
         print("nem sikerult")
+
 teszt_esetek()
